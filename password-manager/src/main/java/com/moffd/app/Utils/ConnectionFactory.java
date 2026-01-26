1411564fc00eebec29187ae13bf2f7292ad1029c
Daniel Moffett <moffd234@gmail.com>
2026-01-26 09:48:30 -0500

    Created ConnectionFactory

---
+++ password-manager/src/main/java/com/moffd/app/Utils/ConnectionFactory.java
@@ -0,0 +1,26 @@
+package com.moffd.app.Utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+    public static Connection getConnection() {
+        final String url = "jdbc:mysql://localhost:3306/jdbcdao";
+        final String username = "dan";
+        final String password = System.getenv("DB_PWD");
+
+        if (password == null) {
+            System.out.println("Error getting db pwd");
+            return null;
+        }
+
+        try{
+            return DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            System.out.println("Error creating db connection " + e);
+        }
+
+        return null;
+    }
+}
