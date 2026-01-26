366902f01fa6e35bb0cfbb8ad3e5147a042db45e
Daniel Moffett <moffd234@gmail.com>
2026-01-26 08:52:35 -0500

    Wrote Credential class

---
+++ password-manager/src/main/java/com/moffd/app/Models/Credential.java
@@ -0,0 +1,57 @@
+package com.moffd.app.Models;
+
+public class Credential {
+    private int id;
+    private int userId;
+    private String site;
+    private String siteUsername;
+    private String sitePassword;
+
+    public Credential(int id, int userId, String site, String siteUsername, String sitePassword) {
+        this.id = id;
+        this.userId = userId;
+        this.site = site;
+        this.siteUsername = siteUsername;
+        this.sitePassword = sitePassword;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getSite() {
+        return site;
+    }
+
+    public void setSite(String site) {
+        this.site = site;
+    }
+
+    public String getSiteUsername() {
+        return siteUsername;
+    }
+
+    public void setSiteUsername(String siteUsername) {
+        this.siteUsername = siteUsername;
+    }
+
+    public String getSitePassword() {
+        return sitePassword;
+    }
+
+    public void setSitePassword(String sitePassword) {
+        this.sitePassword = sitePassword;
+    }
+}
