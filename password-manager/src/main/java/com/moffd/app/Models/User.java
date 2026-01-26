c44f1e001443c9e68e7f7a94fe5d5017f9dc074a
Daniel Moffett <moffd234@gmail.com>
2026-01-26 08:46:17 -0500

    Added getters and setters to User

--- password-manager/src/main/java/com/moffd/app/Models/User.java
+++ password-manager/src/main/java/com/moffd/app/Models/User.java
@@ -5,4 +5,43 @@ public class User {
     private String username;
     private String masterPassword;
     private String email;
+
+    public User(int id, String username, String masterPassword, String email) {
+        this.id = id;
+        this.username = username;
+        this.masterPassword = masterPassword;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getMasterPassword() {
+        return masterPassword;
+    }
+
+    public void setMasterPassword(String masterPassword) {
+        this.masterPassword = masterPassword;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
