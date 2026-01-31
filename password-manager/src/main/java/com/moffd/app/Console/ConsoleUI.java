a7ae6e5cdfa4b52cf5ec087e248f4ba78a44ba3d
Daniel Moffett <moffd234@gmail.com>
2026-01-31 07:00:06 -0500

    Wrote validateEmail

--- password-manager/src/main/java/com/moffd/app/Console/ConsoleUI.java
+++ password-manager/src/main/java/com/moffd/app/Console/ConsoleUI.java
@@ -47,10 +47,11 @@ public class ConsoleUI {
      * - At least 26 character
      * - At least one digit
      * - At least one special character (@#$%^&+=!)
+     *
      * @param password The string to check
      * @return null if valid. Otherwise, returns a description of why its invalid
      */
-    private String isPasswordValid(String password){
+    private String validatePassword(String password) {
         if (password.length() < 26) {
             return "Password must be at least 26 characters long.";
         }
@@ -67,6 +68,21 @@ public class ConsoleUI {
             return "Password must contain at least one special character (@#$%^&+=!).";
         }
         return null;
+    }
+
+    /**
+     * Checks if a given email meets valid email formatting
+     *
+     * @param email string to check for valid email format
+     * @return null if valid. Otherwise, returns a description of why its invalid
+     */
+    private String validateEmail(String email) {
+        if (email == null || email.isEmpty()) {
+            return "Email cannot be empty";
+        }
+
+        String regex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+        return email.matches(regex) ? "Email is not a valid format" : null;
     }
 
 }
