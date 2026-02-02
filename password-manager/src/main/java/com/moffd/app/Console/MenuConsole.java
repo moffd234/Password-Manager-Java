196ee268d3f6e67680cc9903738bb6e823f0cc63
Daniel Moffett <moffd234@gmail.com>
2026-02-02 11:07:43 -0500

    Wrote MenuConsole.run()

--- password-manager/src/main/java/com/moffd/app/Console/MenuConsole.java
+++ password-manager/src/main/java/com/moffd/app/Console/MenuConsole.java
@@ -3,11 +3,34 @@ package com.moffd.app.Console;
 import com.moffd.app.Models.User;
 import com.moffd.app.Utils.IOConsole;
 
+import java.util.List;
+
 public class MenuConsole {
     User currentUser;
     IOConsole console;
 
-    public MenuConsole(IOConsole console) {
+    public MenuConsole(User currentUser, IOConsole console) {
+        this.currentUser = currentUser;
         this.console = console;
+    }
+
+    public void run() {
+        String choice = console.getValidInput("[ credentials ], [ account ], [ logout ]",
+                List.of("credentials", "account", "logout"));
+
+        switch (choice) {
+            case "credentials":
+                new CredentialConsole(currentUser, console).run();
+                break;
+
+            case "account":
+                new AccountConsole(currentUser, console).run();
+                break;
+
+            case "logout":
+                console.printError("logging out...");
+                return;
+
+        }
     }
 }
