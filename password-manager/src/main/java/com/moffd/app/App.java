5851d982da016bc907ede8cba880f35194bc1bf6
Daniel Moffett <moffd234@gmail.com>
2026-02-02 11:13:48 -0500

    Wrote App.main() method

--- password-manager/src/main/java/com/moffd/app/App.java
+++ password-manager/src/main/java/com/moffd/app/App.java
@@ -1,10 +1,24 @@
 package com.moffd.app;
 
+import com.moffd.app.Console.AuthConsole;
+import com.moffd.app.Console.MenuConsole;
+import com.moffd.app.Models.User;
+import com.moffd.app.Utils.IOConsole;
+
 /**
- * Hello world!
+ * Application entry point
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        IOConsole console = new IOConsole();
+        AuthConsole authConsole = new AuthConsole(console);
+
+        while(true) {
+            User user = authConsole.authenticate();
+
+            if (user != null) {
+                new MenuConsole(user, console).run();
+            }
+        }
     }
 }
