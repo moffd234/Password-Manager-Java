ba75b115371ed1f0dc8435223cb0eb40148a5c06
Daniel Moffett <moffd234@gmail.com>
2026-02-02 20:20:17 -0500

    Made CredentialConsole fields final

--- password-manager/src/main/java/com/moffd/app/Console/CredentialConsole.java
+++ password-manager/src/main/java/com/moffd/app/Console/CredentialConsole.java
@@ -1,15 +1,23 @@
 package com.moffd.app.Console;
 
+import com.moffd.app.Dao.CredentialDao;
+import com.moffd.app.Models.Credential;
 import com.moffd.app.Models.User;
 import com.moffd.app.Utils.IOConsole;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CredentialConsole {
-    private User currentUser;
-    private IOConsole console;
+    private final User currentUser;
+    private final IOConsole console;
+    private final CredentialDao credentialDao;
 
     public CredentialConsole(User currentUser, IOConsole console) {
         this.currentUser = currentUser;
         this.console = console;
+        this.credentialDao = new CredentialDao();
     }
 
     public void run() {
