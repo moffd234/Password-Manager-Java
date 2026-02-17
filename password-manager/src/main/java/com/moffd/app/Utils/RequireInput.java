6bf6828e362eba59cb2b2d8036157c8db1dd4384
Daniel Moffett <moffd234@gmail.com>
2026-02-16 22:28:07 -0500

    Wrote requireField

--- password-manager/src/main/java/com/moffd/app/Utils/RequireInput.java
+++ password-manager/src/main/java/com/moffd/app/Utils/RequireInput.java
@@ -3,6 +3,13 @@ package com.moffd.app.Utils;
 import java.util.concurrent.CancellationException;
 
 public class RequireInput {
+
+    /**
+     * Takes an input string and throws a cancellation exception if the input is null or 'back' (case ignored)
+     * @param input The string to check for null or 'back'
+     * @return the inputted string if field is valid. Otherwise, throws a cancellation exception.
+     * @throws CancellationException if input is null or back
+     */
     public static String requireField(String input) throws CancellationException {
 
         if (input == null || input.equalsIgnoreCase("back")) {
